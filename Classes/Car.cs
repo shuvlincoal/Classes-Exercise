c0da56b2c5460efe4e8f38b9543e5da4f9d611f2
mangosax <mcguerty@att.net>
2023-09-05 14:38:16 -0500

    Cars n such

---
+++ Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Car
+    {
+
+        //fields
+
+        public int    Year  { get; set; }  //property
+        public string Model { get; set; }  //property
+        public string Make { get; set; }  //property
+
+
+        public Car(string make, string model, int year)  //custom Constructor
+        {
+            Model = model;
+            Make = make;
+            Year = year;
+
+        }
+
+    }//class
+}//namespace
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+            Car car1   = new Car("Nissan", "Rogue", 2023);
+
+
+            Console.WriteLine($"Car: Make: {car1.Make}, Model: {car1.Model}, Year{car1.Year} ");
+
+
+
+        }//method
+    }//class
+}//namespace
